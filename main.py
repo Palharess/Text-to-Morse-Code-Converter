c59261a8c8e0fdf280c1174c27bf37b54fd7c3dd
Gabriel Palhares <gabrielpalhares334@gmail.com>
2023-04-21 10:57:50 -0300

    Final project

---
+++ main.py
@@ -0,0 +1,29 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+print("Welcome to the Convertor\n")
+text = input("What's the text you want to convert? ")
+converted_text = ""
+
+for letter in text:
+    try:
+        converted_text += MORSE_CODE_DICT[letter.upper()]
+    except KeyError:
+        converted_text += " "
+    except:
+        converted_text += MORSE_CODE_DICT[letter]
+
+
+print(f"The converted text is: {converted_text}")
